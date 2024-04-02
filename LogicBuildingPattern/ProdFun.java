882fe0e84b395ea313fe81006ecf0946fe981eaa
freezeava <aashishava301@gmail.com>
2024-04-02 13:25:09 +0530

    prod function

---
+++ LogicBuildingPattern/ProdFun.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ProdFun {
+
+
+    public static int prod(int a,int b){
+
+        return a*b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two nos a & b : ");
+
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        
+        System.out.println(prod(a,b));
+    }
+    
+}
